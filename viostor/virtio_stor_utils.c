1c9c71cfa7fa88a81330dc1456831a565043a489
Ivan Velickovic <i.velickovic@unsw.edu.au>
2026-02-16 16:59:34 +1100

    viostor: enable DBG and COM_DEBUG
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- viostor/virtio_stor_utils.c
+++ viostor/virtio_stor_utils.c
@@ -91,7 +91,7 @@ void InitializeDebugPrints(IN PDRIVER_OBJECT DriverObject, PUNICODE_STRING Regis
     // TBD - Read nDebugLevel and bDebugPrint from the registry
     bDebugPrint = 1;
     virtioDebugLevel = 0;
-    nViostorDebugLevel = TRACE_LEVEL_ERROR; // TRACE_LEVEL_VERBOSE;//
+    nViostorDebugLevel = TRACE_LEVEL_VERBOSE; // TRACE_LEVEL_VERBOSE;//
 
 #if defined(PRINT_DEBUG)
     VirtioDebugPrintProc = DebugPrintFunc;
